78a59fde8c2f7ba98ea1a3dee39e391d5965146c
Gábor <79107438+Markus1004@users.noreply.github.com>
2023-04-28 17:16:02 +0200

    Update main.php

--- code/main.php
+++ code/main.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="styles.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous">
-        <title>Szimpla Kert</title>
+        <title>Szimpla  Kert</title>
     </head>
     <body>
         <h1>Szimpla Kert!</h1>
