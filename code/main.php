8ffb3e6f4f6ab08a56f0ef87f901913bfac0f5d0
Szabi104 <lekoszabika12@gmail.com>
2023-04-28 18:32:48 +0200

    others

--- code/main.php
+++ code/main.php
@@ -3,7 +3,7 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charset="UTF-8">
-        <link rel="stylesheet" type="text/css" href="styles.css">
+        <link rel="stylesheet" type="text/css" href="others/styles.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous">
         <title>Szimpla Kert</title>
     </head>
