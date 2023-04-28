ecd856d97725d653bfd18a98796e21518a6153a6
Szabi104 <lekoszabika12@gmail.com>
2023-04-28 17:37:49 +0200

    navbar

--- code/main.php
+++ code/main.php
@@ -8,6 +8,9 @@
         <title>Szimpla Kert</title>
     </head>
     <body>
-        <h1>Szimpla Kert!!!</h1>
+        
+        <?php include_once('navbar.php')?>
+        <h1>Szimpla Kert</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     </body>
 </html>
