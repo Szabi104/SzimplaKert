a624c3655fd057901e691a42b8828fa420d4cb45
Szabi104 <lekoszabika12@gmail.com>
2023-04-28 17:55:10 +0200

    s

--- code/main.php
+++ code/main.php
@@ -8,9 +8,8 @@
         <title>Szimpla Kert</title>
     </head>
     <body>
-        
-        <?php include_once('others/navbar.php')?>
         <h1>Szimpla Kert</h1>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+        <?php include_once('others/navbar.php')?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     </body>
 </html>
