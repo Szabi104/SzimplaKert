42c1f5d5dd6d0f1833488e3a145d96a38acacae0
Szabi104 <lekoszabika12@gmail.com>
2023-04-28 16:58:37 +0200

    hey

---
+++ code/main.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="hu">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="styles.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous">
+        <title>Main</title>
+    </head>
+    <body>
+        <h1>Hello!</h1>
+    </body>
+</html>
