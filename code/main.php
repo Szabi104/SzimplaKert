56ff18707163247b93acb1db5d5d81ba4656d9cc
Markus1004 <csontos.gaborka@gmail.com>
2023-04-28 17:13:30 +0200

    Cím változtatás

--- code/main.php
+++ code/main.php
@@ -5,9 +5,9 @@
         <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="styles.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous">
-        <title>Main</title>
+        <title>Szimpla Kert</title>
     </head>
     <body>
-        <h1>Hello!</h1>
+        <h1>Szimpla Kert!</h1>
     </body>
 </html>
