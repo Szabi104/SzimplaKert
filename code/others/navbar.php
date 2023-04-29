7ba63a78e75cf04c9883a9b10bb2a43e317236fa
Markus1004 <csontos.gaborka@gmail.com>
2023-04-29 14:24:21 +0200

    terkep+navbar

---
+++ code/others/navbar.php
@@ -0,0 +1,1 @@
+<?php include_once('others/navbar.php')?>
--- code/websites/productions.php
+++ code/websites/productions.php
@@ -3,7 +3,7 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charset="UTF-8">
-        <link rel="stylesheet" type="text/css" href="/others/styles.css">
+        <link rel="stylesheet" type="text/css" href="./styles.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous">
         <title>Szimpla Kert</title>
     </head>
@@ -14,10 +14,10 @@
     <h1>Szimpla Kert</h1>
   </li>
   <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Főoldal</a>
+    <a class="nav-link"  href="../index.php">Főoldal</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="./websites/productions.php">Termékek</a>
+    <a class="nav-link active" aria-current="page" href="./productions.php">Termékek</a>
   </li>
   <li class="nav-item">
     <a class="nav-link" href="#">Kapcsolat</a>
@@ -28,16 +28,11 @@
 </ul>
   </nav>
 
+  Termékek
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 
-        <footer>
-    <div>
-            
-            
-            <p style="">E-mail: ?</p>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2726.3561783802575!2d19.665990065531457!3d46.895713144806415!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc8292b3f6dc69e7f!2sPallasz+Ath%C3%A9n%C3%A9+Egyetem+GAMF+Kar!5e0!3m2!1shu!2shu!4v1506438875228" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-        </div>
-    </footer>
+    
 
     </body>
 </html>
