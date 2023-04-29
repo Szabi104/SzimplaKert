8298469f5f58dac3f32df7ba9813cb5853e94756
Markus1004 <csontos.gaborka@gmail.com>
2023-04-29 15:35:00 +0200

    oldalak

--- code/main.php
+++ code/main.php
@@ -8,27 +8,8 @@
         <title>Szimpla Kert</title>
     </head>
     <body style="background-color: rgb(179, 255, 179)">
-    <nav  class="navbar" style="background-color: rgb(0, 179, 0)">
-  <ul class="nav nav-underline">
-  <li>
-    <h1>Szimpla Kert</h1>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Főoldal</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="./websites/productions.php">Termékek</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Kapcsolat</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Bejelentkezés/Regisztráció</a>
-  </li>
-</ul>
-  </nav>
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <?php include_once('others/navbar.php')?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>     
 
         <footer>
     <div>
