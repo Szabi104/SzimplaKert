51620fcd196c829379f143246a2b2975a349c2cb
Szabi104 <lekoszabika12@gmail.com>
2023-04-29 22:54:04 +0200

    n

--- code/others/config.inc.php
+++ code/others/config.inc.php
@@ -1,7 +1,10 @@
 <?php
 
 $menu = array(
-    array('text' => 'Home', 'link' => '/exam/index.php'),
-    array('text' => 'Users', 'link' => '/exam/users.php'),
+    array('text' => 'Home', 'link' => '/exam/main.php'),
+    array('text' => 'Reglog', 'link' => '/exam/reglog.php'),
+    array('text' => 'Kapcsolatok', 'link' => '/exam/kapcsolat.php'),
+    array('text' => 'Kosar', 'link' => '/exam/kosar.php'),
+    array('text' => 'Termekek', 'link' => '/exam/termekek.php'),
 );
 ?>
