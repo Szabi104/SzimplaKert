75005149e64a84b173d7125634c063fb1050e021
Szabi104 <lekoszabika12@gmail.com>
2023-05-28 00:25:58 +0200

    s

--- code/others/config.inc.php
+++ code/others/config.inc.php
@@ -1,10 +1,11 @@
 <?php
 
 $menu = array(
-    array('text' => 'SzimplaKert', 'link' => '/project/szimplakert/code/main.php'),
-    array('text' => 'Bejelentkezés/Regisztráció', 'link' => '/project/szimplakert/code/websites/reglog.php'),
-    array('text' => 'Kapcsolatok', 'link' => '/project/szimplakert/code/websites/kapcsolat.php'),
+    array('text' => 'SzimplaKert', 'link' => '/project/SzimplaKert/code/main.php'),
+    array('text' => 'Bejelentkezés/Regisztráció', 'link' => 'D:\programs\htdocs\project\SzimplaKert\code\websites\reglog.php'),
+    array('text' => 'Kapcsolatok', 'link' => '/project/SzimplaKert/code/websites/kapcsolat.php'),
     array('text' => 'Kosar', 'link' => 'websites/kosar.php'),
     array('text' => 'Termekek', 'link' => 'websites/termekek.php'),
 );
 ?>
+
