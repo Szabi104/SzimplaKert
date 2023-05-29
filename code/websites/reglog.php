d58e5900e391c1a028d996e6f1a0c835d297190d
Markus1004 <csontos.gaborka@gmail.com>
2023-05-29 18:32:16 +0200

    igazitasok

--- code/websites/reglog.php
+++ code/websites/reglog.php
@@ -22,13 +22,27 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bejelentkezés/Regisztráció</title>
+    <link rel="stylesheet" href="/SzimplaKert/code/others/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   </head>
   <body>
-
   <?php include_once('../others/navbar.php')?>
+    
 
-  <div class="container">
+
+  
+
+<div class="regloghatter">
+
+</div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+
+<div class="container">
 <form action="" method="POST">
     <div class="form-group">
         <label for="">Name</label>
@@ -76,10 +90,3 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
 
 ?>
 </div>
-
-   
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-  </body>
-</html>
-
